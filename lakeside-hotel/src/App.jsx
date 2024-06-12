1d627fc78169d63da06c5351d7dd68901056262a
anuwatdev245 <tox.anw.6114@gmail.com>
2024-06-12 09:43:19 +0700

    first commit

---
+++ lakeside-hotel/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddRoom from './components/room/AddRoom'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
+
+const App = () => {
+  return (
+    <AddRoom />
+  )
+}
+
+export default App
